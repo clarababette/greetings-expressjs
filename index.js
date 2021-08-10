dc09beed59465dd31617ba469b94395786198152
clarababette <clara_coetzee@zoho.com>
2021-08-10 19:18:37 +0200

    another test

--- index.js
+++ index.js
@@ -6,6 +6,27 @@ import flash from 'express-flash';
 import session from 'express-session';
 const app = express();
 const greetMe = new GreetEveryone();
+const {Client} = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+client.connect();
+
+client.query(
+  'SELECT table_schema,table_name FROM information_schema.tables;',
+  (err, res) => {
+    if (err) throw err;
+    for (const row of res.rows) {
+      console.log(JSON.stringify(row));
+    }
+    client.end();
+  }
+);
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
