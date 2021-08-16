27bd190e5e4fdca6b8ca51742176c49ed6edb746
clarababette <clara_coetzee@zoho.com>
2021-08-16 08:37:52 +0200

    test test

--- test/greet.test.js
+++ test/greet.test.js
@@ -22,28 +22,6 @@ describe('The greetings app', () => {
     assert.strictEqual(['Martin'], users);
   });
 
-  // it('should retain the count after refreshing', () => {
-
-  // });
-  // it('should be able to reset the count', () => {
-
-  // });
-  // it('should be able to greet someone in English', () => {
-
-  // });
-  // it('should be able to greet someone in Kiswahili', () => {
-
-  // });
-  // it('should be able to greet someone in Hungarian', () => {
-
-  // });
-  // it('should greet someone in the language of their choosing', () => {
-
-  // });
-
-  // it('should be able to count each person greeted only once', () => {
-
-  // });
   after(function() {
     pool.end();
   });
